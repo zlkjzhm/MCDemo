70f00f3d11134b807a93f6d544d556d63868dc02
zhanghuanmeng <zlkj_zhm@163.com>
2020-10-24 16:01:55 +0800

    添加项目文件。

---
+++ Models/MCFillingModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCDemo.Models
+{
+    class MCFillingModel
+    {
+        //填料片属性
+        private double _length;         //单位：mm
+        private double _width;          //单位：mm
+        private double _density;        //单位：g/cm3
+        private double _thickness;      //单位：mm
+        private double _waveDistance;   //单位：mm
+        public MCFillingModel(double length, double width, double density, double thickness, double waveDistance)
+        {
+            //_length = 1000;
+            //_width = 500;
+            //_density = 1.45;
+            //_thickness = 0.45;
+            //_waveDistance = 34;
+            _length = length;
+            _width = width;
+            _density = density;
+            _thickness = thickness;
+            _waveDistance = waveDistance;
+
+        }
+
+        double MassCalculate()
+        {
+
+            return 0;
+        }
+    }
+}
